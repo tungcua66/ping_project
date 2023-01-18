d4eda820c2d9c6e6d2fbcb46386086b3a5967d95
tung <tunghuong66bg@gmail.com>
2023-01-19 00:52:07 +0100

    initital project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () =>  {
+  const [ping, setPing] = useState(0);
+  const [inputValue, setInputValue] = useState("https://ping.zenetys.com/api");
+  const [interval, setInterval] = useState(1000);
+  const [isRunning, setIsRunning] = useState(false);
+  const onClickHandler = async(interval) => {
+    // try {
+    //   const startTime = Date.now();
+    //   const baseUrl = inputValue;
+    //   await axios.get(baseUrl, {
+    //     crossDomain: true
+    //   })
+    //   const endTime = Date.now();
+    //   setPing(endTime - startTime);
+    //   setStop(false);
+    // } catch (error) {
+    // console.error(error.message);
+    // }
+    // setI
+  }
+
+  const pingServer = async () => {
+    try {
+        const startTime = Date.now();
+        const baseUrl = inputValue;
+        await axios.get(baseUrl, {
+          crossDomain: true
+        })
+        const endTime = Date.now();
+        setPing(endTime - startTime);
+      } catch (error) {
+      console.error(error.message);
+      }
+  };
+
+  useEffect(() => {
+    console.log('isrunning', isRunning);
+    if (isRunning) {
+        const intervalId = setInterval(() => {
+          console.log('hello')
+        }, 100);
+        return () => clearInterval(intervalId);
+    }
+}, [isRunning]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> welcome to ping app</h1>
+      <input type="text" 
+          value={inputValue} 
+          onChange={e => setInputValue(e.target.value)}
+          placeholder="https://www.google.com"
+      />
+      <input type="text" 
+          value={interval} 
+          onChange={e => setInterval(e.target.value)}
+          placeholder="interval input(ms)"
+      />
+
+      <button onClick={() => setIsRunning((prev) => !prev)}>
+                {isRunning ? 'Stop' : 'Start'}
+      </button>
+      {ping && <div> Ping: {ping} ms</div>}
+
+    </>
   );
 }
 
